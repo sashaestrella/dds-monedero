1ad174218fe05ddd595ea639aa38a398592d7e51
Sasha Belen Estrella <sestrella@lanacion.com.ar>
2022-05-12 14:23:12 -0300

    agrego metodos en Cuenta para evitar long method

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -31,15 +31,14 @@ public class Cuenta {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
 
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (cantidadDepositosDiarios() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
+    Deposito unDeposito = new Deposito(LocalDate.now(), cuanto);
+    movimientos.add(unDeposito);
     saldo += cuanto;
-    Extraccion unaExtraccion = new Extraccion(LocalDate.now(), cuanto);
-    movimientos.add(unaExtraccion);
   }
-
 
   public void sacar(double cuanto) {
     if (cuanto <= 0) {
@@ -48,20 +47,30 @@ public class Cuenta {
     if (getSaldo() - cuanto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
-    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
-    double limite = 1000 - montoExtraidoHoy;
+
+    double limite = calcularLimiteDiario();
+
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
 
-    saldo -= cuanto;
     Extraccion unaExtraccion = new Extraccion(LocalDate.now(), cuanto);
     movimientos.add(unaExtraccion);
+    saldo -= cuanto;
   }
 
   public void agregarMovimiento(Movimiento movimiento) {
     movimientos.add(movimiento);
+  }
+
+  private long cantidadDepositosDiarios() {
+    return getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count();
+  }
+
+  private double calcularLimiteDiario() {
+    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+    return 1000 - montoExtraidoHoy;
   }
 
   public double getMontoExtraidoA(LocalDate fecha) {
